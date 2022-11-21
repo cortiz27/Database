d32bf5c383f1f1cc870ca824bf8222e934d67ec5
cortiz27 <ortizchristian10@gmail.com>
2022-11-21 14:48:27 -0500

    Initial Correction to Create Statements and Script to Insert Values into Users table.

---
+++ InsertUsers.sql
@@ -0,0 +1,42 @@
+INSERT INTO Users VALUES
+(100000, 'John','Smith','1992-01-13',	'JohnSmith@gmail.com',	'310-324-5566',	'Los Angeles',	'Plant Dad. Father of 2. Techie.',	FALSE,	FALSE);
+INSERT INTO Users VALUES
+(111100, 'Robert','Zepeda','1985-03-19',	'RobertZepeda@gmail.com',	'215-323-7624',	'Los Angeles',	'Proud Small Business Owner of a LA-based Beer Garden. Open to Hosting Vendors on Select Days of the week.',	FALSE,	TRUE);
+INSERT INTO Users VALUES
+(111111, 'Oscar','Green','1998-06-24',	'OscarGreen@gmail.com',	'215-323-6742',	'Los Angeles',	'UCLA Grad Student. On the search for classic vinyl records.',	FALSE,	FALSE);
+INSERT INTO Users VALUES
+(120000, 'Tyler','Ali','1996-04-03',	'TylerAli@gmail.com',	'310-232-5432',	'Los Angeles',	'Custom Art Vendor. Open to commisions and currently selling my latest collection of pieces inspired by COVID 19 and the diaspora.',	TRUE,	FALSE);
+INSERT INTO Users VALUES
+(122200, 'Eve','Lancaster','2001-12-21',	'EveLancaster@gmail.com',	'206-523-6648',	'Los Angeles',	'Custom Jewelry Designer. LA Parks and Rec Administrator open to have vendors contact me for information regarding holding markets in local parks.',	TRUE,	TRUE);
+INSERT INTO Users VALUES
+(123000, 'Theresa','Sanders','1994-09-14',	'TheresaSanders@gmail.com',	'971-323-7658',	'Los Angeles',	'Antique Collector.',	FALSE,	FALSE);
+INSERT INTO Users VALUES
+(123300, 'Julia','Fox','2001-07-25',	'JuliaFox@gmail.com',	'215-208-7622',	'Los Angeles',	'Custom Rug and Carpet Weaver.',	TRUE,	FALSE);
+INSERT INTO Users VALUES
+(123333, 'Trevor','Noah','2001-02-27',	'TrevorNoah@gmail.com',	'312-543-9988',	'Los Angeles',	'Star Trek Nerd & Comic Vendor.',	TRUE,	FALSE);
+INSERT INTO Users VALUES
+(123400, 'Catherine','Pine','2002-03-24',	'CatherinePine@gmail.com',	'312-608-7788',	'Los Angeles',	NULL,	FALSE,	FALSE);
+INSERT INTO Users VALUES
+(123455, 'Jon','Snow','1992-05-12',	'JonSnow@gmail.com',	'415-233-7804',	'Los Angeles', NULL,	FALSE,	TRUE);
+INSERT INTO Users VALUES
+(123456, 'Sarah','Parker','2001-03-27',	'SarahParker@gmail.com',	'408-323-4312',	'Los Angeles',	NULL, FALSE, FALSE);
+INSERT INTO Users VALUES
+(110000, 'Jane','Doe','1995-04-14',	'JaneDoe@gmail.com',	'646-474-3214',	'New York City',	'NYC Thrifter. Always on the hunt for a good club fit :)',	FALSE,	FALSE);
+INSERT INTO Users VALUES
+(111000, 'James','Silver','2000-04-12',	'JamesSilver@gmail.com',	'512-425-3452',	'New York City',	'Hand Crafted Candle Vendor',	TRUE,	FALSE);
+INSERT INTO Users VALUES
+(111110, 'Lisa','Song','1992-05-06',	'LisaSong@gmail.com',	'512-424-3355',	'New York City',	'Bronx Mother of 3. Looking to host vendors in our local community garden venue.',	FALSE,	TRUE);
+INSERT INTO Users VALUES
+(122000, 'Christopher','Wilde','1992-03-11',	'ChristopherWilde@gmail.com',	'206-318-7654',	'New York City',	'First Edition Book Collector.', 	FALSE,	FALSE);
+INSERT INTO Users VALUES
+(122220, 'Julia','Roberts','2000-04-10',	'JuliaRoberts@gmail.com',	'971-788-5564',	'New York City',	'Juliard\'s Dance Student. I offer private dance lessons and career advice for aspriring professional dancers.',	TRUE,	FALSE);
+INSERT INTO Users VALUES
+(122222, 'Amy','Nguyen','1996-08-18',	'AmyNguyen@gmail.com',	'415-600-3201',	'New York City',	'Serving the best traditional Ho Chi Minh-Style Pho!',	TRUE,	FALSE);
+INSERT INTO Users VALUES
+(123330, 'Timothy','O\'Brien','1999-11-17',	'TimothyOBrien@gmail.com',	'646-212-3710',	'New York City',	NULL,	FALSE,	FALSE);
+INSERT INTO Users VALUES
+(123440, 'Juan','Lopez','1975-12-21',	'JuanLopez@gmail.com',	'408-929-2562',	'New York City',	'Come try the best tacos in NYC!',	TRUE,	FALSE);
+INSERT INTO Users VALUES
+(123444, 'Julian','Thiam','2000-04-30',	'JulianThiam@gmail.com',	'512-422-3441',	'New York City',	NULL,	FALSE,	FALSE);
+INSERT INTO Users VALUES
+(123450, 'Rebecca','Pena','1999-07-27',	'RebeccaPena@gmail.com',	'408-713-4462',	'New York City',	NULL,	FALSE,	FALSE);
