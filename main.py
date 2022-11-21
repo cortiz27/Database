c6e27f5b736e8ce37554623441404238200da820
cortiz27 <ortizchristian10@gmail.com>
2022-11-21 15:55:05 -0500

    Starter Code

---
+++ main.py
@@ -0,0 +1,30 @@
+import pymysql
+
+"""
+If you type in your password down below after running the SQL Files in the MySQL WorkBench, it should work.
+"""
+
+def mysqlconnect():
+    # To connect MySQL database
+    conn = pymysql.connect(
+        host= 'localhost',
+        user='root',
+        password = "",
+        database="ToteDB",
+    )
+
+    cur = conn.cursor()
+
+    # Select query
+    cur.execute("select * from Users")
+    output = cur.fetchall()
+
+    for i in output:
+        print(i)
+
+    # To close the connection
+    conn.close()
+
+# Driver Code
+if __name__ == "__main__" :
+    mysqlconnect()
